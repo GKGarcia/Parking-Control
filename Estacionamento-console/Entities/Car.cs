3c78764ab443b2c2a41ed909256f67e81ad41aa8
GKGarcia <gabrielkrutsch963@gmail.com>
2024-02-11 09:50:17 -0300

    implementation of methods

--- Estacionamento-console/Entities/Car.cs
+++ Estacionamento-console/Entities/Car.cs
@@ -15,18 +15,171 @@ namespace Estacionamento_console.Entities
 
         public string EntryTime { get; set; }
         public string ExitTime { get; set; }
+        List<Car> list = new List<Car>();
         public Car() { }
 
-        public Car(string name, string cpf, string model, Color color, string plate, string entrytime) : base(name, cpf)
+        public Car(string name, string cpf, string model, Color color, string plate) : base(name, cpf)
         {
             Model = model;
             Color = color;
             Plate = plate;
-            EntryTime = entrytime;
+            entrytime();
         }
 
-       
+        public void Menu()
+        {
+            Console.WriteLine("BEM VINDO AO MENU ");
+            Console.WriteLine("1- Cadastrar Motorista");
+            Console.WriteLine("2- Veiculos Estacionados");
+            Console.WriteLine("3- Registrar Saida de Veiculo");
+            Console.WriteLine("4- Buscar Veiculos");
+            Console.WriteLine("5- Listar Veiculos");
+            Console.WriteLine("6- SAIR");
+            Console.Write("Digite uma opção:");
+            
 
+          
+        }
+       public void CadastrarMoto()
+        {
+            Console.WriteLine("CADASTRO DE MOTORISTA:");
+            Console.WriteLine();
+            Console.WriteLine("Digite o nome do motorista:");
+
+            string Name = Console.ReadLine();
+            Console.WriteLine("Digite o CPF do motorista:");
+            string Cpf = Console.ReadLine();
+            Console.WriteLine("Digite o modelo do veiculo:");
+            string Model = Console.ReadLine();
+            Console.WriteLine("Digite a cor do veiculo:");
+            Color Color = Enum.Parse<Color>(Console.ReadLine());
+            Console.WriteLine("Digite a placa do veiculo:");
+            string Plate = Console.ReadLine();
+            Console.Write("Data e Horario de Entrada:");
+
+            list.Add(new Car(Name, Cpf, Model, Color, Plate));
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Veiculo Cadastrado!");
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void veiculosestacionados()
+        {
+            Console.WriteLine("VEICULOS ESTACIONADOS:");
+            Console.WriteLine();
+
+
+            foreach (var obj in list)
+            {
+
+                if (obj.ExitTime == null)
+                {
+                    Console.WriteLine(obj);
+
+
+                    Console.WriteLine();
+                }
+
+            }
+
+
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void RegistrarSaida()
+        {
+            Console.WriteLine("Digite o nome do motorista ou a placa do carro:");
+            string Exit = Console.ReadLine();
+            Console.WriteLine();
+            foreach (var obj in list)
+            {
+                if (Exit == obj.Plate && obj.ExitTime == null || Exit == obj.Name && obj.ExitTime == null)
+                {
+                    Console.WriteLine("MOTORISTA ENCONTRADO");
+                    Console.WriteLine();
+                    Console.WriteLine(obj);
+                    Console.WriteLine();
+
+                    Console.WriteLine("Tem certeza que deseja registrar a saida?(s/n)");
+                    char decision = char.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (decision == 's' || decision == 'S')
+                    {
+                        Console.WriteLine("Saida Registrada!");
+                        obj.RegisterExit();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Registro de saida cancelado!");
+                    }
+
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+            }
+            Console.WriteLine();
+
+        }
+
+        public void buscarveiculos()
+        {
+            Console.WriteLine("Digite o nome do motorista ou a placa do carro:");
+            string search = Console.ReadLine();
+            foreach (var obj in list)
+            {
+                if (search == obj.Name || search == obj.Plate)
+                {
+                    Console.WriteLine("Motorista Encontrado!");
+                    Console.WriteLine(obj);
+                }
+                else
+                {
+                    Console.WriteLine("Placa ou motorista invalidos!");
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        public void listarveiculos()
+        {
+            Console.WriteLine("Lista de Veiculos:");
+            Console.WriteLine();
+
+            foreach (var obj in list)
+            {
+                if (obj.ExitTime != null)
+                {
+                    Console.WriteLine(obj);
+                    Console.WriteLine("Horario de saida:" + obj.ExitTime);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Saidas de veiculos ainda não registrada!");
+                }
+
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void entrytime()
+        {
+            EntryTime = DateTime.Now.ToString();
+            Console.WriteLine(EntryTime);
+        }
         public Car(string exittime)
         {
             ExitTime = exittime;
--- Estacionamento-console/Program.cs
+++ Estacionamento-console/Program.cs
@@ -12,161 +12,43 @@ namespace Estacionamento_console
     {
         static void Main(string[] args)
         {
-            List<Car> list = new List<Car>();
+            
             Car car = new Car();
             Driver driver = new Driver();
 
-            string name; 
-
+            string name;
             int opcao = 0;
 
             while (opcao != 6)
             {
-
-                Console.WriteLine("BEM VINDO AO MENU ");
-                Console.WriteLine("1- Cadastrar Motorista");
-                Console.WriteLine("2- Veiculos Estacionados");
-                Console.WriteLine("3- Registrar Saida de Veiculo");
-                Console.WriteLine("4- Buscar Veiculos");
-                Console.WriteLine("5- Listar Veiculos");
-                Console.WriteLine("6- SAIR");
-                Console.Write("Digite uma opção:");
+                
+                car.Menu();
                 opcao = int.Parse(Console.ReadLine());
-
+                
                 Console.Clear();
 
                 switch (opcao)
                 {
                     case 1:
 
-
-                        Console.WriteLine("CADASTRO DE MOTORISTA:");
-                        Console.WriteLine();
-                        Console.WriteLine("Digite o nome do motorista:");
-                        
-                        string Name = Console.ReadLine();
-                        Console.WriteLine("Digite o CPF do motorista:");
-                        string Cpf = Console.ReadLine();
-                        Console.WriteLine("Digite o modelo do veiculo:");
-                        string Model = Console.ReadLine();
-                        Console.WriteLine("Digite a cor do veiculo:");
-                        Color Color = Enum.Parse<Color>(Console.ReadLine());
-                        Console.WriteLine("Digite a placa do veiculo:");
-                        string Plate = Console.ReadLine();
-                        Console.WriteLine("Data e Horario de Entrada:");
-                        string EntryTime = Console.ReadLine();
-
-                        list.Add(new Car(Name,Cpf,Model,Color,Plate,EntryTime));
-
-
-                        Console.WriteLine();
-                        Console.WriteLine("Veiculo Cadastrado!");
-
-                        Console.ReadKey();
-                        Console.Clear();
+                        car.RegisterDriver();
+                      
                         break;
                     case 2:
-                        
-                        Console.WriteLine("VEICULOS ESTACIONADOS:");
-                        Console.WriteLine();
 
-                       
-                            foreach (var obj in list)
-                            {
-
-                                if (obj.ExitTime == null)
-                                {
-                                    Console.WriteLine(obj);
-                                
-                               
-                                    Console.WriteLine();
-                            }
-                          
-                            }
-                       
-                        
-
-                        Console.ReadKey();
-                        Console.Clear();
+                        car.ParkedVehicles();
                         break;
                     case 3:
-                        Console.WriteLine("Digite o nome do motorista ou a placa do carro:");
-                        string Exit = Console.ReadLine();
-                        Console.WriteLine();
-                        foreach (var obj in list)
-                        {
-                            if (Exit == obj.Plate && obj.ExitTime == null || Exit == obj.Name && obj.ExitTime == null)
-                            {
-                                Console.WriteLine("MOTORISTA ENCONTRADO");
-                                Console.WriteLine();
-                                Console.WriteLine(obj);
-                                Console.WriteLine();
-
-                                Console.WriteLine("Tem certeza que deseja registrar a saida?(s/n)");
-                                char decision = char.Parse(Console.ReadLine());
-                                Console.WriteLine();
-
-                                if (decision == 's'|| decision =='S')
-                                {
-                                    Console.WriteLine("Saida Registrada!");
-                                    obj.RegisterExit();
-
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Registro de saida cancelado!");
-                                }
-
-                                Console.ReadKey();
-                                Console.Clear();
-
-                            }
-                        }
-                        Console.WriteLine();
-
+                        car.Exit();
                         break;
                     case 4:
 
-                        Console.WriteLine("Digite o nome do motorista ou a placa do carro:");
-                        string search = Console.ReadLine();
-                        foreach (var obj in list)
-                        {
-                            if (search == obj.Name || search == obj.Plate)
-                            {
-                                Console.WriteLine("Motorista Encontrado!");
-                                Console.WriteLine(obj);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Placa ou motorista invalidos!");
-                            }
-                            Console.ReadKey();
-                            Console.Clear();
-                        }
+                       car.SearchVehicles();
 
                         break;
                     case 5:
 
-                        Console.WriteLine("Lista de Veiculos:");
-                        Console.WriteLine();
-
-                        foreach (var obj in list)
-                        {
-                            if (obj.ExitTime!= null)
-                            {
-                                Console.WriteLine(obj);
-                                Console.WriteLine("Horario de saida:" + obj.ExitTime);
-                                Console.WriteLine();
-                            }
-                            else
-                            {
-                                Console.WriteLine("Saidas de veiculos ainda não registrada!");
-                            }
-                          
-                        }
-
-                        Console.ReadKey();
-                        Console.Clear();
+                       car.ListVehicles();
                         break;
 
                 }
